425b3a5b86de3729b5ab78825a72df3ef91fb459
nidheeshmd <md.nidheesh3@gmail.com>
2020-08-18 06:46:44 +0200

    usereducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, {useState, useReducer, useEffect, useCallback} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -6,11 +6,53 @@ import ErrorModal from '../UI/ErrorModal';
 
 import Search from './Search';
 
-const Ingredients = () => {
+const ingredientReducer = (currentIngredients, action) => {
+  switch(action.type){
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id);
+      default:
+        throw new Error('should not get there!');
+  }
+};
 
-  const [UserIngredients, setUserIngredients] = useState([]);
-  const [IsLoading, setIsLoading] = useState(false);
-  const [AppError, setAppError] = useState();
+const httpReducer = (curHttpState, action) => {
+  switch(action.type){
+    case 'SEND':
+      return {
+        loading: true, error: null
+      };
+    case 'RESPONSE':
+      return{
+        ...curHttpState, loading: false
+      };
+    case 'ERROR':
+      return {
+        loading: false, error: action.errorMessage
+      };
+    case 'CLEAR':
+      return{...curHttpState, error: null}
+    default:
+    throw new Error('End of switch');
+  }
+};
+
+const Ingredients = () => {
+const [UserIngredients, dispatch]=useReducer(ingredientReducer,[]);
+const [httpState, dispatchHttp] = useReducer(httpReducer, {loading:false, error: null});
+/* useReducer takes two arguments 1st one is our reducer class(here: ingredientReducer)
+and second one (optional argument) is starting state. here we passing empty array as currentIngredients at first 
+useReducer returns an array with 2 elements. first one is a state(here: UserIngredients)
+second one is a function that handle by the reducer. we can put any name to the 
+function.
+here we call second function 'dispatch' instead of 'setUserIngredients'*/
+
+  //const [UserIngredients, setUserIngredients] = useState([]);
+  //const [IsLoading, setIsLoading] = useState(false);
+  //const [AppError, setAppError] = useState();
 
   //useEffect will work after and every component renter.
   /* useEffect always have two arguments.1st one is the function that execute after every rendered cycle.
@@ -38,23 +80,29 @@ const Ingredients = () => {
   },[UserIngredients]);
 
   const addIngredientHandler = (ingredient) => {
-    setIsLoading(true);
+    //setIsLoading(true);
+dispatchHttp({type:'SEND'}); //this will automatically set loading is true and error is null
     fetch('https://react-hook-app-34f37.firebaseio.com/ingredients.json',
-    {method:'POST', 
+    {method:'POST',
     body: JSON.stringify(ingredient),
     headers:{'Content-Type' : 'application/json'}
     }).then(response => {
-      setIsLoading(false);
+      //setIsLoading(false);
+      dispatchHttp({type:'RESPONSE'});
      return response.json();
     }).then(responseData =>{
-      setUserIngredients((prevIngredients) => [
-        ...prevIngredients,
-        {id: responseData.name, ...ingredient}]);
+      //setUserIngredients((prevIngredients) => [
+      //  ...prevIngredients,
+      //  {id: responseData.name, ...ingredient}]);
+      dispatch({
+        type: 'ADD', 
+        ingredient:{id: responseData.name, ...ingredient}});
     });
   };
 
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
-      setUserIngredients(filteredIngredients)
+      //setUserIngredients(filteredIngredients);
+      dispatch({type: 'SET', ingredients: filteredIngredients});
   },[]);
 
   /* useCallback have 2 argument, first one is our function and 2nd one is 
@@ -62,29 +110,34 @@ const Ingredients = () => {
   if [] then execute only once. */
 
   const removeIngredientHandler = ingredientId => {
-    setIsLoading(true);
-    fetch(`https://react-hook-app-34f37.firebaseio.com/ingredients/${ingredientId}.jon`,
+    //setIsLoading(true);
+    dispatchHttp({type:'SEND'});
+    fetch(`https://react-hook-app-34f37.firebaseio.com/ingredients/${ingredientId}.json`,
     {method:'DELETE'
     }).then(response => {
-      setUserIngredients(prevIngredients =>
-        prevIngredients.filter(ingredient => ingredient.id !==ingredientId))
-        setIsLoading(false);
+      dispatchHttp({type:'RESPONSE'});
+      //setUserIngredients(prevIngredients =>
+      //prevIngredients.filter(ingredient => ingredient.id !==ingredientId));
+        dispatch({type: 'DELETE', id:ingredientId});
+        //setIsLoading(false);
     }).catch( error => {
-      setAppError(error.message);
+      //setAppError(error.message);
+      dispatchHttp({type: 'ERROR', errorMessage: error.message});
     });
   };
 
   const clearError = () => {
-    setAppError(null);
-    setIsLoading(false);
+    //setAppError(null);
+    //setIsLoading(false);
+    dispatchHttp({type: 'CLEAR'});
   }
 
   return (
     <div className="App">
-      {AppError ? <ErrorModal onClose={clearError}>{AppError}</ErrorModal> : null}
+      {httpState.error ? <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal> : null}
       <IngredientForm 
       onAddIngredient = {addIngredientHandler}
-      loading = {IsLoading}/>
+      loading = {httpState.loading}/>
 
       <section>
         <Search onLoadIngredients ={filteredIngredientsHandler}/>
